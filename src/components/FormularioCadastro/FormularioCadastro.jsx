9791924d61cb1c3ad8211de0e5649bb4e368be98
annykfernandes <anny.klarice.fernandes.127@ufrn.edu.br>
2021-08-12 15:45:12 -0300

    add course project

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,60 @@
+import { Step, StepLabel, Stepper, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import DadosEntrega from "./DadosEntrega";
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
+
+function FormularioCadastro({ aoEnviar }) {
+  const [etapaAtual, setEtapaAtual] = useState(0);
+  const [dadosColetados, setDados] = useState({});
+
+  useEffect(() => {
+    if (etapaAtual === formularios.length) {
+      aoEnviar(dadosColetados);
+    }
+  });
+
+  const formularios = [
+    <DadosUsuario aoEnviar={coletarDados} />,
+    <DadosPessoais aoEnviar={coletarDados} />,
+    <DadosEntrega aoEnviar={coletarDados} />,
+    <Typography variante="h5">Obrigado pelo cadastro!</Typography>,
+  ];
+
+  function coletarDados(dados) {
+    setDados({ ...dadosColetados, ...dados });
+    proximo();
+  }
+
+  function proximo() {
+    setEtapaAtual(etapaAtual + 1);
+  }
+
+  return (
+    <>
+      <Stepper activeStep={etapaAtual}>
+        <Step>
+          <StepLabel>Login</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Dados pessoais</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Entrega</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Finalização</StepLabel>
+        </Step>
+      </Stepper>
+      {formularios[etapaAtual]}
+    </>
+  );
+}
+
+{
+  /* <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
+<DadosUsuario />
+<DadosEntrega/> */
+}
+
+export default FormularioCadastro;
